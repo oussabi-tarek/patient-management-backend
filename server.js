abbd58f4587569f286b5d790b102e820c5402d65
oussabi-tarek <tarek.oussabi@edu.uca.ma>
2023-12-07 15:20:34 +0100

    refact:code refactoring

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express=require('express');
 const cors=require('cors');
-const axios=require('axios');
 
 const app = express();
 var corsOptions = {
@@ -10,23 +9,7 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.post("/authenticate", async (req, res) => {
-    const { username } = req.body;
-    console.log(username);
-    // Get or create user on Chat Engine!
-    try {
-      const r = await axios.put(
-        "https://api.chatengine.io/users/",
-        { username: username, secret: username, first_name: username },
-        { headers: { "Private-Key": "a0c23d88-ea10-4733-80f3-7aa7a9ab7006" } }
-      );
-      return res.status(r.status).json(r.data);
-    } catch (e) {
-      return res.status(e.response.status).json(e.response.data);
-    }
-  });
-
-
+require('./routes/chat.routes')(app);
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
